cc239128442e7aa24e9ca956551b84f7aa655bdc
Kevin Richey <kevin.richey@gmail.com>
2017-06-28 18:15:17 -0400

    Cleaning up files

--- build.rb
+++
@@ -1,8 +0,0 @@
-projectFile = ARGV[0]
-outputFile = ARGV[1]
-
-file = File.new(projectFile)
-lines = file.readlines()
-filesToCompile = lines.inject("") { |result,line| result + " " + line.strip }
-system("g++ -std=c++0x #{filesToCompile} -pedantic `sdl-config --cflags --libs` -o bin/#{outputFile}")
-
