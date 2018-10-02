12c105e45b31bbf62e6fbf20d52d6fa623895b98
Kevin Richey <kevin.richey@gmail.com>
2018-10-02 11:44:23 -0400

    Cleaned up, removed complex functoid stuff

--- test.cpp
+++ test.cpp
@@ -14,11 +14,19 @@ class Trace
 {
   public:
     explicit Trace(SourceLine sourceline) : source(sourceline) {}
+    virtual void print(const char* category, const char* message);
 
   protected:
     SourceLine source;
     static int level;
 };
+
+void Trace::print(const char* category, const char* message)
+{
+    printf("%s:%d:\tL%d\t%s %s\n", 
+           source.file, source.line, level,
+           category, message);
+}
 
 int Trace::level = 0;
 
@@ -37,18 +45,12 @@ ScopeTrace::ScopeTrace(SourceLine sourceline, const char* scopename) :
   scope  (scopename)
 {
     ++level;
-
-    printf("%s:%d:\tL%d\t%s %s\n", 
-           source.file, source.line, level,
-           "begin", scope);
+    print("begin", scope);
 }
 
 ScopeTrace::~ScopeTrace()
 {
-    printf("%s::\tL%d\t%s %s\n", 
-           source.file, level,
-           "end", scope);
-
+    print("end", scope);
     --level;
 }
 
@@ -61,9 +63,7 @@ class Checkpoint : public Trace
 Checkpoint::Checkpoint(SourceLine sourceline, const char* message) :
   Trace (sourceline)
 {
-    printf("%s:%d:\tL%d\t%s %s\n", 
-           source.file, source.line, level,
-           "checkpoint", message);
+    print("checkpoint", message);
 }
 
 class Watch : public Trace
@@ -73,110 +73,20 @@ class Watch : public Trace
     Watch(SourceLine sourceline, const char* name, const T& value) :
       Trace(sourceline)
     {
-        printf("%s:%d:\tL%d\t%s %s = %d\n", 
-               source.file, source.line, level,
-               "trace", name, value);
+        char text[256];
+        size_t length = sizeof(text)/sizeof(char);
+        snprintf(text, length, "%s = %d", name, value);
+        print("watch", text);
     }
 };
 
 class Failure
 {
-};
-
-class TracePrinter
-{
   public:
-    TracePrinter() =default;
-    virtual ~TracePrinter() =0;
-    virtual void print(SourceLine source, int level, const char* expression) =0;
-    const char* category {};
-};
-
-TracePrinter::~TracePrinter() {}
-void TracePrinter::print(SourceLine source, int level, const char* expression) {}
-
-class TraceStreamPrinter : public TracePrinter {
-  public:
-    void print(SourceLine source, int level, const char* expression) override
-    {
-        printf("%s:%d:\tL%d\t%s %s\n", 
-               source.file, source.line, level,
-               category, expression);
-    }
-};
-
-class IFunctoid {
-  public:
-    virtual void func() =0;
-    virtual ~IFunctoid() =0;
-};
-
-IFunctoid::~IFunctoid() {}
-
-class NullFunctoid : public IFunctoid {
-  public:
-    void func() override;
-};
-
-void NullFunctoid::func() {}
-
-template <typename FunctImp> 
-class WrapFunctoid : public IFunctoid {
-  public:
-    explicit WrapFunctoid(FunctImp* imp) : functoid(imp) {}
-    void func() override { functoid->func(); }
-
-  private:
-    FunctImp* functoid;
-};
-
-class Functoid : public IFunctoid {
-
-    IFunctoid* functoid {};
-
-  protected:
-
-    explicit Functoid(IFunctoid* fn) : functoid(fn) {}
-
-  public:
-
-    Functoid() : functoid(new NullFunctoid()) {}
-
-    template <typename Type>
-    explicit Functoid(Type* obj) : functoid(new WrapFunctoid<Type>(obj)) {}
-
-    explicit Functoid(const Functoid&) =delete;
-    Functoid& operator=(const Functoid&) =delete;
-
-    template <typename Type>
-    Functoid& operator=(Type* obj) 
-    {
-        Functoid(obj).swap(*this);
-        return *this;
-    }
-
-    void swap(Functoid& that)
-    {
-        IFunctoid* oid = that.functoid;
-        that.functoid = functoid;
-        functoid = oid;
-    }
-
-    void func() override { functoid->func(); }
-    void operator()() { func(); }
-
-    ~Functoid() override { delete functoid; }
-};
-
-
-
-
-class ThrowingFunctoid {
-  public:
-    void func() 
-    {
-        throw Failure();
-    }
+    SourceLine source;
+    int level;
+    const char* category;
+    const char* message;
 };
 
 class Assert : public Trace {
@@ -185,26 +95,18 @@ class Assert : public Trace {
       Trace (sourceline)
     {
         if (!condition) {
-            printer->print(source, level, expression);
-            fail();
+            print("assert-fail", expression);
+            fail("assert-fail", expression);
         }
     }
 
-    static void set(TracePrinter* newPrinter);
-    static TracePrinter* printer;
-
-    static Functoid fail;
+    virtual void fail(const char* category, const char* message) const;
 };
 
-Functoid Assert::fail;
-
-void Assert::set(TracePrinter* newPrinter)
+void Assert::fail(const char* category, const char* message) const
 {
-    newPrinter->category = "assert failure";
-    printer = newPrinter;
-}
-
-TracePrinter* Assert::printer {};
+    throw Failure { source, level, category, message };
+};
 
 #define kwr_Assert(condition)  kwr::Assert  kwr_Assert_Trace##__LINE__ ( kwr_Source, (condition), (#condition) )
 
@@ -221,12 +123,6 @@ void puke()
 
 int main(int argc, char* argv[])
 {
-    kwr::TraceStreamPrinter assertPrinter;
-    kwr::Assert::set(&assertPrinter);
-    
-    kwr::ThrowingFunctoid thrower;
-    kwr::Assert::fail = &thrower;
-
     kwr::ScopeTrace appstart( kwr_Source, "test.exe" );
 
     kwr::Checkpoint checkpoint1( kwr_Source, "I'm here");
